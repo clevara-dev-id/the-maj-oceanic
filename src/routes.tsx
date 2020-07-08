f28bc9791db1829edca096a11ef8030842d0edfa
Ridwan Arifin <r.arifin@localhost.localdomain>
2020-07-08 23:02:30 +0700

    modified default route

--- src/routes.tsx
+++ src/routes.tsx
@@ -27,11 +27,8 @@ const BaseRoute: React.FC = () => {
             <Route exact path="/the-vessel/spesification" component={Spesification} />
             <Route exact path="/contact-us" component={Contact} />
             <Route exact path="/sailing-soon" component={Sailing} />
-            <Route path="*" >
-                <Redirect to="/sailing-soon" />
-            </Route>
         </React.Fragment>
     )
 };
 
-export default withRouter(BaseRoute);
+export default BaseRoute;
